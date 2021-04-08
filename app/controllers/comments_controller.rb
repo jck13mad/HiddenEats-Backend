f15a6fdb30409f5567ecd0f280256ec0f70c7b4c
John Madden <jck13mad@gmail.com>
2021-04-08 15:13:52 -0500

    Filled in comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+
+    def index 
+        render json: Comment.all 
+    end
+
+    def create 
+        comment = Comment.new(comment_params)
+
+        if comment.save 
+            render json: comment 
+        else 
+            render json: {errors: comment.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def destroy 
+        Comment.find(params[:id]).destroy 
+    end
+
+    private 
+
+        def comment_params
+            params.require(:comment).permit(:content, :menu_item_id)
+        end
 end
