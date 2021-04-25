a35cb8d15ddc7dd320e5e1f4102821f896117d4c
John Madden <jck13mad@gmail.com>
2021-04-24 22:24:53 -0500

    Final Commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 Item.destroy_all
 Comment.destroy_all 
 
-Item.create(name: "4×4", company: "In-N-Out", image: "https://i2.wp.com/www.eatthis.com/wp-content/uploads/2019/10/4x4-burger.jpg?w=1200&ssl=1", description: "Simply order a Four-by-Four and you'll get this delicious masterpiece, with your favorite toppings and secret sauce included.")
-coldBuster = Item.create(name: "Cold Buster", company: "Starbucks", image: "https://i0.wp.com/www.eatthis.com/wp-content/uploads/media/images/ext/224609746/starbucks-secret-orders-cold-buster-instagram.jpg?w=1024&ssl=1", description: "The Cold Buster is made of half steamed lemonade, half hot water, and a teabag each of Jade Citrus Mint green tea and Peach Tranquility herbal tea.")
+Item.create(id: 1, name: "4×4", company: "In-N-Out", image: "https://i2.wp.com/www.eatthis.com/wp-content/uploads/2019/10/4x4-burger.jpg?w=1200&ssl=1", description: "Simply order a Four-by-Four and you'll get this delicious masterpiece, with your favorite toppings and secret sauce included.")
+coldBuster = Item.create(id: 2, name: "Cold Buster", company: "Starbucks", image: "https://i0.wp.com/www.eatthis.com/wp-content/uploads/media/images/ext/224609746/starbucks-secret-orders-cold-buster-instagram.jpg?w=1024&ssl=1", description: "The Cold Buster is made of half steamed lemonade, half hot water, and a teabag each of Jade Citrus Mint green tea and Peach Tranquility herbal tea.")
 
-Comment.create(content: "Wow this is definitely on my list to check out!", item_id: coldBuster.id)
+Comment.create(id: 1, content: "Wow this is definitely on my list to check out!", item_id: coldBuster.id)
